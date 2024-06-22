fc393ba59a386c277b3a5c05084fffe49c7b6111
emec_delam <kiwi.bp.surviv@gmail.com>
2024-06-22 12:27:42 +0200

    moving docker builds to another .sh to allow user to build the main docker without side docker ( improving docker compile time)

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 ./gradlew shadowJar
 docker build -t eplbot ./
-#docker build -t java-runner -f src/main/java/com/github/hokkaydo/eplbot/module/code/java/Dockerfile .
 docker compose -f docker-compose.yml up
---
+++ build_code_docker.sh
@@ -0,0 +1,1 @@
+docker build -t java-runner -f src/main/java/com/github/hokkaydo/eplbot/module/code/java/Dockerfile .

2e37545dd9b008e42b1336f35ad6aa4111fadbd2
hokkaydo <hokkaydo@users.noreply.github.com>
2024-11-10 17:41:55 +0100

    Add Ralof feur

--- src/main/java/com/github/hokkaydo/eplbot/module/ratio/RatioListener.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/ratio/RatioListener.java
@@ -1,8 +1,6 @@
 package com.github.hokkaydo.eplbot.module.ratio;
 
 import com.github.hokkaydo.eplbot.Main;
-import net.dv8tion.jda.api.entities.MessageReaction;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -23,12 +21,12 @@ public class RatioListener extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         GuildChannel channel = Main.getJDA().getGuildChannelById(517720163223601155L);
         if (channel == null) return;
-        TextChannel textChannel = (TextChannel) channel;
+        /*TextChannel textChannel = (TextChannel) channel;
         textChannel.retrieveMessageById(FUCK_THEM_MESSAGE_ID).queue(m -> m.getReactions().stream().filter(r -> r.getEmoji().getName().equals("epl_Smart")).findFirst().map(MessageReaction::retrieveUsers).ifPresent(a -> a.queue(l -> {
             if(l.stream().anyMatch(u -> u.getIdLong() == event.getAuthor().getIdLong()) && random.nextInt(100) < 10) {
                 event.getMessage().addReaction(Emoji.fromCustom("ratio", RATIO_REACTION_ID, false)).queue();
             }
-        })));
+        })));*/
 
         if(event.getAuthor().getIdLong() == RALOF_ID) {
             event.getMessage().addReaction(Emoji.fromCustom("Feur", FEUR_REACTION_ID, false)).queue();

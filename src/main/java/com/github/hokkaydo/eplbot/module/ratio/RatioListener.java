a97b904318ae1e2401b22226b456605dbbe26d54
hokkaydo <hokkaydo@users.noreply.github.com>
2024-11-10 17:38:54 +0100

    Add Ralof feur

--- src/main/java/com/github/hokkaydo/eplbot/module/ratio/RatioListener.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/ratio/RatioListener.java
@@ -14,6 +14,9 @@ import java.util.Random;
 public class RatioListener extends ListenerAdapter {
 
     private static final Long FUCK_THEM_MESSAGE_ID = 1113769154684653569L;
+    private static final Long RATIO_REACTION_ID = 1113815294541041674L;
+    private static final Long RALOF_ID = 192631146566123520L;
+    private static final Long FEUR_REACTION_ID = 1305209638844891177L;
     private static final Random random = new Random();
 
     @Override
@@ -23,9 +26,13 @@ public class RatioListener extends ListenerAdapter {
         TextChannel textChannel = (TextChannel) channel;
         textChannel.retrieveMessageById(FUCK_THEM_MESSAGE_ID).queue(m -> m.getReactions().stream().filter(r -> r.getEmoji().getName().equals("epl_Smart")).findFirst().map(MessageReaction::retrieveUsers).ifPresent(a -> a.queue(l -> {
             if(l.stream().anyMatch(u -> u.getIdLong() == event.getAuthor().getIdLong()) && random.nextInt(100) < 10) {
-                event.getMessage().addReaction(Emoji.fromCustom("ratio", 1113815294541041674L, false)).queue();
+                event.getMessage().addReaction(Emoji.fromCustom("ratio", RATIO_REACTION_ID, false)).queue();
             }
         })));
+
+        if(event.getAuthor().getIdLong() == RALOF_ID) {
+            event.getMessage().addReaction(Emoji.fromCustom("Feur", FEUR_REACTION_ID, false)).queue();
+        }
     }
 
 }

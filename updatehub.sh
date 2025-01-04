dd1e1cd7505c0712fed983adc9ed735f6adcd597
hokkaydo <hokkaydo@users.noreply.github.com>
2025-01-04 20:56:38 +0100

    Comment useless lines at the moment

--- updatehub.sh
+++ updatehub.sh
@@ -1,11 +1,11 @@
 ./gradlew shadowJar
 docker build -t eplbot:latest --target production ./
-docker build -t eplbot-profiler:latest --target profiler ./
+# docker build -t eplbot-profiler:latest --target profiler ./
 docker image tag eplbot:latest hokkaydo/eplbot:latest
-docker image tag eplbot-profiler:latest hokkaydo/eplbot-profiler:latest
-sh build_code_docker.sh
+# docker image tag eplbot-profiler:latest hokkaydo/eplbot-profiler:latest
+# sh build_code_docker.sh
 docker image push hokkaydo/eplbot:latest
-docker image push hokkaydo/eplbot-profiler:latest
-docker image push hokkaydo/java-runner:latest
-docker image push hokkaydo/c-runner:latest
-docker image push hokkaydo/python-runner:latest
+# docker image push hokkaydo/eplbot-profiler:latest
+# docker image push hokkaydo/java-runner:latest
+# docker image push hokkaydo/c-runner:latest
+# docker image push hokkaydo/python-runner:latest
